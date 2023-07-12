ed0dcd5f8d54186a269b18d230cedfd23f250bc7
Ro444elle <121978303+Ro444elle@users.noreply.github.com>
2023-07-12 18:27:59 +0300

    update1

--- src/api.js
+++ src/api.js
@@ -9,6 +9,20 @@ export async function fetchImages(q, page, perPage) {
     const response = await axios.get(url);
     return response.data;
   } catch (error) {
+    console.log(error);
     throw new Error('Failed to fetch images');
   }
 }
+
+// export const fetchImages = async (q, page, perPage) => {
+//   const URL = 'https://pixabay.com/api/';
+//   const KEY = '38153781-c51513b757834e649365382a3';
+//   const url = `${URL}?key=${KEY}&q=${q}&page=${page}&per_page=${perPage}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+//   try {
+//     const response = await axios.get(url);
+//     return response.data;
+//   } catch (error) {
+//     throw new Error('Failed to fetch images');
+//   }
+// };
