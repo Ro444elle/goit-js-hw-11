95403fc2ecc18a5c4de41bb7bcc87c3754356998
Ro444elle <121978303+Ro444elle@users.noreply.github.com>
2023-07-12 18:56:40 +0300

    update5

--- src/api.js
+++ src/api.js
@@ -2,22 +2,22 @@ import axios from 'axios';
 // import axios, * as others from 'axios';
 
 console.log('start...');
-let xx = fetchImages('dog', 1, 3);
-console.log(xx);
 
-export function fetchImages(q, page, perPage) {
+export async function fetchImages(q, page, perPage) {
   const URL = 'https://pixabay.com/api/';
   const KEY = '38153781-c51513b757834e649365382a3';
-  const url = `${URL}?key=${KEY}&q=${q}&page=${page}&per_page=${perPage}&image_type=photo&orientation=horizontal&safesearch=true`;
+  const xurl = `${URL}?key=${KEY}&q=${q}&page=${page}&per_page=${perPage}&image_type=photo&orientation=horizontal&safesearch=true`;
 
   try {
-    const response = axios.get(url);
+    let response = await axios.get(xurl);
     return response.data;
   } catch (error) {
     console.log(error);
     throw new Error('Failed to fetch images');
   }
 }
+
+console.log('end...');
 
 // export const fetchImages = async (q, page, perPage) => {
 //   const URL = 'https://pixabay.com/api/';
