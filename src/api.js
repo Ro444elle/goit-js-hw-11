ffcdb9be1de333cb244df0bc5016c9649f78edfb
Ro444elle <121978303+Ro444elle@users.noreply.github.com>
2023-07-12 18:47:32 +0300

    update4

--- src/api.js
+++ src/api.js
@@ -5,13 +5,13 @@ console.log('start...');
 let xx = fetchImages('dog', 1, 3);
 console.log(xx);
 
-export async function fetchImages(q, page, perPage) {
+export function fetchImages(q, page, perPage) {
   const URL = 'https://pixabay.com/api/';
   const KEY = '38153781-c51513b757834e649365382a3';
   const url = `${URL}?key=${KEY}&q=${q}&page=${page}&per_page=${perPage}&image_type=photo&orientation=horizontal&safesearch=true`;
 
   try {
-    const response = await axios.get(url);
+    const response = axios.get(url);
     return response.data;
   } catch (error) {
     console.log(error);
